e28784a398cf42c8e108951d3655ce1db33e44af
Luis Manzanero <manzanero.luis1995@gmail.com>
2021-12-28 01:39:11 +0800

    added other pages to be excluded from sitemap

--- front-end/gatsby-config.js
+++ front-end/gatsby-config.js
@@ -22,7 +22,7 @@ module.exports = {
         // Exclude specific pages or groups of pages using glob parameters
         // See: https://github.com/isaacs/minimatch
         // The example below will exclude the single `path/to/page` and all routes beginning with `category`
-        exclude: [`/category/*`, '/app/*'],
+        exclude: [`/category/*`, '/dashboard/', '/login/', '/app/'],
         query: `
           {
             site {
