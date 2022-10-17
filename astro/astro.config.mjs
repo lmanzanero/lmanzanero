60df18f773e98ab285890c1e2515a3e257f0b6ac
Luis Manzanero <manzanero.luis1995@gmail.com>
2022-10-18 04:45:41 +0800

    updated config to fix broken paths

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -6,6 +6,5 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://luismanzanero.com/',
-	integrations: [mdx(), sitemap()],
-	base: '/portfolio'
+	integrations: [mdx(), sitemap()], 
 });
