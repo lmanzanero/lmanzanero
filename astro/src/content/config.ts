1b46e77b1e55c074826ceaff92884fc01c168b33
Luis Manzanero <manzanero.luis1995@gmail.com>
2023-02-07 02:13:13 +0800

    added music collection

--- astro/src/content/config.ts
+++ astro/src/content/config.ts
@@ -10,6 +10,22 @@ const blogCollection = defineCollection({
   })
 });
 
+const musicCollection = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    date: z.string(),
+    tags: z.array(z.string()),
+    file: z.string(),
+    image: z.string(),
+    youtubeUrl: z.string(),
+    soundCloudUrl: z.string(),
+    tikTokUrl: z.string(),
+    twitterUrl: z.string(),
+  })
+})
+
 export const collections = {
-  'blog': blogCollection
+  'blog': blogCollection,
+  'music': musicCollection
 }
