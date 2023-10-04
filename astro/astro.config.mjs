4fad9258036fbaa99b4ef3899c010e756017c011
Luis Manzanero <manzanero.luis1995@gmail.com>
2023-10-04 22:07:35 +0800

    updated project image

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://luismanzanero.com/',
+	site: 'https://luismanzanero.com',
 	integrations: [mdx(), sitemap()], 
 });
